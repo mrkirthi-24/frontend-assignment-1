25a1a2742b9e53b25116e714a2b2bbb0a982bab1
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-11-25 18:20:17 +0530

    create reusable button component

---
+++ eligere-assignment/src/utils/StyledButtons.tsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+
+export const StyledButton = styled(Button)`
+  text-transform: none;
+  width: 100%;
+  color: #132d78;
+  font-size: 0.9rem;
+  font-weight: bold;
+  padding: 0.7rem 0rem;
+  border-radius: 7px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.6);
+`;
+
+export const SortButton = styled(Button)`
+  text-transform: none;
+  color: #132d78;
+  font-size: 0.9rem;
+  font-weight: bold;
+  padding: 0.5rem 1rem;
+  border-radius: 7px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.4);
+`;
+
+export const InvoiceButton = styled(Button)`
+  text-transform: none;
+  width: 100%;
+  color: #132d78;
+  font-size: 0.9rem;
+  padding: 0.7rem 0rem;
+  border-radius: 7px;
+`;
