175a2d890284eb8eb9ad34b465982e8fd56bac7c
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-11-25 13:12:54 +0530

    add grid to homepage

--- eligere-assignment/src/components/Home.tsx
+++ eligere-assignment/src/components/Home.tsx
@@ -1,9 +1,20 @@
+import { Grid } from "@mui/material";
+import LeftSide from "./LeftSIde";
 import NavBar from "./NavBar";
+import RightSide from "./RightSide";
 
 const Home = () => {
   return (
     <>
       <NavBar />
+      <Grid container bgcolor="#eef3fa" height="90vh">
+        <Grid item xs={2}>
+          <LeftSide />
+        </Grid>
+        <Grid item xs={10}>
+          <RightSide />
+        </Grid>
+      </Grid>
     </>
   );
 };
---
+++ eligere-assignment/src/components/LeftSide.tsx
@@ -0,0 +1,5 @@
+const LeftSide = () => {
+  return <div>LeftSIde</div>;
+};
+
+export default LeftSide;
